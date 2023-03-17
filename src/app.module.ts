1e8b89955e2cb46d1649d847b698a2a389bbba6e
Milo2003 <milonorenalo@gmail.com>
2023-03-17 11:42:59 -0500

    Empezamos Curso de NestJs: Autenticacion con passport y jwt: Generamos nuestro primer guardian y el modulo auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import axios from 'axios';
 import { environments } from './environments';
+import { AuthModule } from './auth/auth.module';
 import config from './config';
 
 @Module({
@@ -28,6 +29,7 @@ import config from './config';
     ProductsModule,
     HttpModule,
     DatabaseModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [
